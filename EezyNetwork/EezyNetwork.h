17c4e09fbc325b393ed078b15b53a77f471e5adb
Чингиз Куандык <k.chyngiz@gmail.com>
2020-01-30 11:18:29 +0600

    Initial Commit

---
+++ EezyNetwork/EezyNetwork.h
@@ -0,0 +1,19 @@
+//
+//  EezyNetwork.h
+//  EezyNetwork
+//
+//  Created by Shyngys Kuandyk on 1/30/20.
+//  Copyright © 2020 Shyngys Kuandyk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EezyNetwork.
+FOUNDATION_EXPORT double EezyNetworkVersionNumber;
+
+//! Project version string for EezyNetwork.
+FOUNDATION_EXPORT const unsigned char EezyNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EezyNetwork/PublicHeader.h>
+
+
